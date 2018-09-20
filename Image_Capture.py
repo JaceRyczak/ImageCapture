779350855d6f06b6c71a63efe2ad599bfd9fb3a3
JaceRyczak <jaryczak@gmail.com>
2018-09-20 14:03:56 +1000

    cleaned up and commented

--- Image_Capture.py
+++ Image_Capture.py
@@ -1,49 +1,60 @@
-import cv2 
+import cv2  
 import time
 import numpy as np
 
-hysteresis = True 
+#Variable Declaration
+cap_enable = False 
 img_counter = 0
+delta_best = 5
 
+#Threshold Settings
+thrs_still = 2
+thrs_move = 5
+
+# define function for doing image difference calculation.
 def diffImg(t0, t1, t2):
   d1 = cv2.absdiff(t2, t1)
   d2 = cv2.absdiff(t1, t0)
   return cv2.bitwise_and(d1, d2)
  
+# select video feed device and set desired image size and frequency of feed.
 cam = cv2.VideoCapture(0)
- 
-winName = "Movement Indicator"
-cv2.namedWindow(winName)
-winName1 = "Actual Image"
-cv2.namedWindow(winName1)
- 
-# Read three initial images
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1024)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 768)
+cam.set(cv2.CAP_PROP_FPS, 30)
+print("FPS:", cam.get(cv2.CAP_PROP_FPS))
+ret, img_hold = cam.read() 					
+
+# Read three initial images and convert to 1D
 img1 = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
 img2 = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
 img3 = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
-ret, img_hold = cam.read()
 
 while True:
-	img_counter += 1
-	cv2.imshow( winName, diffImg(img1, img2, img3) )
-	average = np.average(diffImg(img1, img2, img3))
-	print("average is:", average)
-	if average < 2 and hysteresis:
+	# calculate differential image average pixel value	
+	img_delta = np.average(diffImg(img1, img2, img3))		
+	#if average value of difference is less than threshold, enable capture	
+	if img_delta < thrs_still:						
+		cap_enable = True
+	# once enabled, compare newest image stillness with best historical image stillness and if better, save image for writing later.	
+	if cap_enable:
+		if img_delta < delta_best:
+			delta_best = img_delta
+			img_best = img_hold
+	# once image has movement again, save the stillest image captured and reset flags for next time movement stops.
+	if img_delta > thrs_move and cap_enable:
+		img_counter += 1		
 		img_name = "test_image{}.png".format(img_counter)
-		cv2.imwrite(img_name, img_hold)
-		hysteresis = False
-	if average > 5:
-		hysteresis = True
-	# Read next image
+		cv2.imwrite(img_name, img_best)
+		cap_enable = False
+		delta_best = thrs_move
+	
+	# Read next images for next loop.
 	img1 = img2
 	img2 = img3
 	img3 = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
 	ret, img_hold = cam.read()
-	cv2.imshow( winName1, img_hold)
-	
-	key = cv2.waitKey(10)
-	if key == 27:
-    		cv2.destroyWindow(winName)
-    		break
- 
+	 
 print("Goodbye")
+
+
--- resolution.py
+++ resolution.py
@@ -3,17 +3,35 @@ import time
 
 cam = cv2.VideoCapture(0)
  
-winName = "Movement Indicator"
-cv2.namedWindow(winName)
+#winName = "Movement Indicator"
+#cv2.namedWindow(winName)
 
-print("width:", cv2.cv.CV_CAP_PROP_FRAME_WIDTH)
-
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+cam.set(cv2.CAP_PROP_FPS, 30)
+#cam.set(cv2.CAP_PROP_CONVERT_RGB, False)
+print("Width:", cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+print("Height:", cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+print("FPS:", cam.get(cv2.CAP_PROP_FPS))
 while True:
-	time.sleep(0.5)
+	#time.sleep(0.5)
 	ret, img_hold = cam.read()
-	cv2.imshow( winName, img_hold)
+	#cv2.imshow( winName, img_hold)
 
-	key = cv2.waitKey(10)
-	if key == 27:
-    		cv2.destroyWindow(winName)
-    		break
+	#key = cv2.waitKey(10)
+	#if key == 27:
+    	#	cv2.destroyWindow(winName)
+    	#	break
+
+#create and name some windows to display realtime images (for commissioning only)
+#winName = "Movement Indicator"
+#cv2.namedWindow(winName)
+#winName1 = "Actual Image"
+#cv2.namedWindow(winName1)
+
+#	cv2.imshow( winName, diffImg(img1, img2, img3) )
+#	cv2.imshow( winName1, img_hold)	
+#	img_delta = np.average(diffImg(img1, img2, img3))
+#	print("Image stillness is:", img_delta)
+#	print("cap_enable = ", cap_enable)
+#	print("delta best = ", delta_best)
